94554851206760d537d3ad33e063f48704d2b63b
devtuanamit1409 <nguyentuanamit@gmail.com>
2025-03-06 18:29:37 +0700

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -28,17 +28,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=AW-16896445095"
-      ></script>
-      <script
         dangerouslySetInnerHTML={{
           __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-16896445095');
-            `,
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NF2C32J8');
+          `,
         }}
       />
       <body
